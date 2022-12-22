fa911f260a8d8d03e2ff61a623e5b391be1e1065
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2022-12-22 23:25:34 +0000

    created initial commit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,87 @@
-// use moment
-// search history on the left
-// check lower casing, like the movie app
-// open weather api, current and 5 day
-// use jquery to create the wireframing layout
-// use hover for the buttons and filter the information if you can
-// arrays has a filter by value function
-//region check
-// current weather
-// media query to adjust for screen size
-// alert for city not found
-// prevent default if using a form
-//  2 colum lay out
-// degree = &deg inject into DOM
-//  api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=apiKey - 5day
-// $.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid={apiKey}&units=metric
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} - geo query - current day
-// `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}`
-var apiKey = '2477ba37b5fe48f255ae88cf1a2662ae';
-var city = 'London'
+var apiKey = "a87f8badc77fd8896923e7a24622a25f";
+var city = "London";
+var baseURL = "https://api.openweathermap.org/data/2.5/";
+var currentURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
+var forecastURL = baseURL + `forecast?appid=${apiKey}&units=metric&`;
+var iconURL = "https://openweathermap.org/img/w/";
+var currentDateEl = $(".current-date");
+var searchBtn = $("#search-button");
+var searchInput = $(".search-input");
+var today = $("#today");
+var foreCast = $("#forecast");
+var cardWrapper =$('main')
+//Add current date
 
-   
-// 
+console.log(searchBtn);
 
-$.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-    .then(function(currentData) {
-        var lon = currentData.coord.lon;
-        var lat = currentData.coord.lat;
+searchBtn.click(function () {
+  inputSubmitted(searchInput.val());
+});
 
+function inputSubmitted(cityName) {
+  $.get(currentURL + `q=${cityName}`).then(function (currentData) {
+    console.log(currentData)
+    today.html("");
+    // console.log(currentData);
+    // console.log(currentData.name);
+    var currentDay = moment().format("dddd Do MMMM YYYY");
+    today.append(currentDay);
+    var nameEl = $("<h1></h1>");
+    nameEl.text(currentData.name);
+    today.append(nameEl);
+    var iconEl = (`<img src='${iconURL + currentData.weather[0].icon}.png'>`);
+    today.append(iconEl);
+    var tempEl = $("<h3>");
+    tempEl.text(`Temp: ${Math.round(currentData.main.temp)}°C`);
+    today.append(tempEl);
+    var humEl = $("<h3>");
+    humEl.text(`Humidity: ${currentData.main.humidity}%`);
+    today.append(humEl);
+    var windEl = $("<h3>");
+    windEl.text(`Wind: ${currentData.wind.speed}`);
+    today.append(windEl);
 
-        console.log(`
-        ------Current Conditions-------
-        Temp: ${Math.round(currentData.main.temp)}°C
-        Wind: ${currentData.wind.speed} M/S
-        Humidity: ${currentData.main.humidity}
-        `);
+    // console.log(`
+    //     Temp: ${Math.round(currentData.main.temp)}°C
+    //     Humidity: ${currentData.main.humidity}%
+    //     Wind: ${currentData.wind.speed}
+    //     IconURL: ${iconURL + currentData.weather[0].icon}.png;
+    //     `);
 
-        $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
-        .then(function (forecastData){
-            console.log(forecastData);
-        })
+      $.get(forecastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`
+        ).then(function (forecastData) {
+            for (var castObj of forecastData.list) {
+                cardWrapper.append(`
+                <div class="weather-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),;">
+                    <h3>${castObj.dt_txt}</h3>    
+                    <img src='${iconURL + castObj.weather[0].icon.png}'>
+                    <h3>Temp: ${castObj.main.temp}°C</h3>
+                    <h3>Wind: ${castObj.wind.speed}m/s</h3>
+                    <h3>Humidity: ${castObj.main.humidity}%</h3>       
+                </div>`)
+                console.log(forecastData.list)
+        }
     });
+});
+}
+
+// $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+//     .then(function(currentData) {
+//         var lon = currentData.coord.lon;
+//         var lat = currentData.coord.lat;
+
+//         console.log(`
+//         ------Current Conditions-------
+//         Temp: ${Math.round(currentData.main.temp)}°C
+//         Wind: ${currentData.wind.speed} M/S
+//         Humidity: ${currentData.main.humidity}
+//         `);
+
+//         $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+//         .then(function (forecastData){
+//             console.log(forecastData);
+//         })
+//     });
 
 // // no jquery version
 
