96426ab3397859466f23f8311126080dfc72dd5d
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2023-01-25 20:43:37 +0300

    corrected the append

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,24 +9,17 @@ var searchInput = $(".search-input");
 var today = $("#today");
 var foreCast = $("#forecast");
 var cardWrapper = $('#forecast');
-var localSearches = [];
+var searchHistory = $('#search-history');
 
 console.log(searchBtn);
 
 searchBtn.click(function () {
   inputSubmitted(searchInput.val());
+  searchFunction();
+  
   searchInput.val('');
+  
 });
-
-// function inputSubmitted(event) {
-//     var keyCode = event.keyCode;
-//     var searchText = searchInput.val();
-// console.log(searchInput)
-//     if (keyCode === 13 && searchText) {
-
-//     }
-// }
-
 
 function inputSubmitted(cityName) {
   $.get(currentURL + `q=${cityName}`).then(function (currentData) {
@@ -49,27 +42,41 @@ function inputSubmitted(cityName) {
     var windEl = $("<h3>");
     windEl.text(`Wind: ${currentData.wind.speed}`);
     today.append(windEl);
-
-        
-
-
-      $.get(forecastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`
-        ).then(function (forecastData) {
-            for (var i = 0; i < forecastData.list.length; i+=8) {
-                cardWrapper.append(`
-                <div class="weather-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),;">
-                    <h3>${forecastData.list[i].dt_txt}</h3>    
-                    <img src='${iconURL + forecastData.list[i].weather[0].icon}.png'>
-                    <h3>Temp: ${forecastData.list[i].main.temp}°C</h3>
-                    <h3>Wind: ${forecastData.list[i].wind.speed}m/s</h3>
-                    <h3>Humidity: ${forecastData.list[i].main.humidity}%</h3>       
-                </div>`)
-                console.log(forecastData.list)
-        }
+    
+    $.get(forecastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`
+    ).then(function (forecastData) {
+      cardWrapper.html('');
+      for (var i = 0; i < forecastData.list.length; i+=8) {
+        cardWrapper.append(`
+        <div class="weather-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),;">
+        <h3>${forecastData.list[i].dt_txt}</h3>    
+        <img src='${iconURL + forecastData.list[i].weather[0].icon}.png'>
+        <h3>Temp: ${forecastData.list[i].main.temp}°C</h3>
+        <h3>Wind: ${forecastData.list[i].wind.speed}m/s</h3>
+        <h3>Humidity: ${forecastData.list[i].main.humidity}%</h3>       
+        </div>`)
+        localStorage.setItem(forecastData, JSON.stringify(forecastData));
+        console.log(forecastData.list)
+      }
     });
-});
+  });
+}
+function searchFunction(data) {
+  console.log($('#search').val());
+  localStorage.setItem(searchHistory, JSON.stringify(searchHistory));
+  searchHistory.push($('#search').val());
+  // $('#search').val('');
+  $('#search-history').text('');
+
+  $.each(searchHistory, function (index, value){
+    $('#search-history').append("<button class='historyItem' onclick='addtotextbox("+index+")'>" + value + '</button')
+    console.log(value);
+  });
 }
 
+// function addtotextbox(id) {
+//   $('.search-input').val(searchHistory(id));
+// }
 
 
 
